e271cd4eb16af83c22e6c73765a7bf7c8ae0028c
Sonny Guiam Jr <sonnyg@qualisph.com>
2021-05-17 06:57:16 +0800

    Leave Application. Change Modal title according to function.

--- httpdocs/admin/leave_application.php
+++ httpdocs/admin/leave_application.php
@@ -332,7 +332,7 @@ include ('../includes/fetchData.php');
                                     <div class="modal-dialog modal-lg" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel"> View Leave</h5>
+                                                <h5 class="modal-title" id="exampleModalLabel"> Edit Leave</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">×</span>
                                                 </button>
